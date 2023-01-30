d1ca672b46bb004aced16848e0a2e9f59cc91c31
alexbotha <alexbotha@hotmail.co.uk>
2023-01-30 13:27:35 +0000

    commit tree c7d7d3474a6710f430bec85f0517bc39f8ec542a

--- chatterbox-client/src/components/App.js
+++ chatterbox-client/src/components/App.js
@@ -11,8 +11,10 @@ function App() {
   const [messages, setMessages] = useState([]);
   const [search, setSearch] = useState("");
 
+  
+
   useEffect(() => {
-    fetch("http://localhost:4000/messages")
+    fetch("http://localhost:9292/messages")
       .then((r) => r.json())
       .then((messages) => setMessages(messages));
   }, []);
